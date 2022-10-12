c144414e9ec99f94172f0c574bd322da4470a2d5
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-10-12 16:18:13 +0530

    working on regex validator

---
+++ RegexValidator2/RegexValidator2/RegexValidator2.h
@@ -0,0 +1,18 @@
+//
+//  RegexValidator2.h
+//  RegexValidator2
+//
+//  Created by Akshay Kumar on 12/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegexValidator2.
+FOUNDATION_EXPORT double RegexValidator2VersionNumber;
+
+//! Project version string for RegexValidator2.
+FOUNDATION_EXPORT const unsigned char RegexValidator2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexValidator2/PublicHeader.h>
+
+
---
+++ RegexValidator2/RegexValidator2/RegexValidator2.swift
@@ -0,0 +1,14 @@
+//
+//  RegexValidator2.swift
+//  RegexValidator2
+//
+//  Created by Akshay Kumar on 12/10/22.
+//
+
+import Foundation
+
+public class RegexValidator2 {
+    public static func checkEmailRegex(email: String){
+        print("===Regext==\(email)")
+    }
+}
