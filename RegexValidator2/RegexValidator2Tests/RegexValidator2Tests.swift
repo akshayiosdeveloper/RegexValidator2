67230cd1129296a079c40083cbb703295ccd4302
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-10-12 21:12:08 +0530

    Added new test case

---
+++ RegexValidator2/RegexValidator2Tests/RegexValidator2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  RegexValidator2Tests.swift
+//  RegexValidator2Tests
+//
+//  Created by Akshay Kumar on 12/10/22.
+//
+
+import XCTest
+
+final class RegexValidator2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+    
+    func testExample2() throws {
+        XCTAssertEqual(3*4, 12)
+    }
+    
+    func testExample3() throws {
+        XCTAssertEqual(10+10, 20)
+    }
+
+
+}
